49c3305efe2046de4cd5360e7f70bba434b2e106
Elielson Andre <elielsonandre123@gmail.com>
2024-03-09 11:11:49 -0300

    login page

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -4,14 +4,9 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
-            <Link href="/">
-            <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
-            </Link>
-        </div>
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white">
 
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        <div class="w-full max-w-4xl mt-6 px-10 py-4  bg-white sm:shadow-lg overflow-hidden sm:rounded-lg ">
             <slot />
         </div>
     </div>
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -6,6 +6,8 @@ import InputLabel from '@/Components/InputLabel.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import TextInput from '@/Components/TextInput.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
+// import { UserStrokeIcon } from '@heroicons/vue/24/solid'
+import { TheMask } from 'vue-the-mask'
 
 defineProps({
     canResetPassword: {
@@ -31,64 +33,75 @@ const submit = () => {
 
 <template>
     <GuestLayout>
+
         <Head title="Log in" />
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
+        <div class=" w-full flex  h-screen sm:h-auto">
+
+            <div class="w-1/2 hidden sm:block ">
+                <img src="@/Images/AuthIllustration.png" class="w-full" />
+            </div>
+            <div class="w-full sm:w-1/2 flex flex-col justify-between">
+
+                <div class="w-full">
+                    <div class="w-full mb-16 flex justify-center sm:hidden">
+                        <img src="@/Images/logo.png" class="w-44" />
+                    </div>
+
+                    <span class="mb-1.5 block text-gray-500 font-medium">Olá novamente!👋</span>
+                    <h2 class="mb-9 text-2xl font-bold text-gray-500 sm:text-title-xl2">
+                        Acesse seu painel
+                    </h2>
+
+                    <div class="mb-4">
+                        <label class="mb-2.5 block font-medium text-black ">Digite seu CPF:</label>
+
+                        <label class="input input-bordered flex items-center gap-2">
+                            <input v-mask="'###.###.###-##'" type="text"
+                                class="grow text-xl border-none focus:ring-0 shadow-none"
+                                placeholder="___.___.___-__" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                            </svg>
+                        </label>
+
+                    </div>
+
+                    <div v-if="true" role="alert" class="alert alert-error mb-2 text-red-600 bg-red-100 ">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6 hidden sm:block"
+                            fill="none" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        <span>Cliente não encontrado. Caso não tenha cadastro <a href="#"
+                                class="text-blue-500 underline">Clique aqui</a></span>
+                    </div>
+
+                    <button
+                        class="btn  w-full bg-blue-500 hover:bg-blue-400  px-4 text-white text-xl font-bold transition-all rounded-xl">
+                        Entrar
+                        <div class="icon">
+                            <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0 0h24v24H0z" fill="none"></path>
+                                <path
+                                    d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                                    fill="currentColor"></path>
+                            </svg>
+                        </div>
+                        <!-- <span class="loading loading-spinner"></span> -->
+                    </button>
+
+                </div>
+                <div class="hidden w-full my-6 sm:flex justify-center">
+                    <img src="@/Images/logo.png" class="w-40" />
+                </div>
+                <small class="text-center text-gray-400">© 2024 - GetCash</small>
+            </div>
+
         </div>
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
-
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ms-2 text-sm text-gray-600">Remember me</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Forgot your password?
-                </Link>
-
-                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
-                </PrimaryButton>
-            </div>
-        </form>
     </GuestLayout>
 </template>
