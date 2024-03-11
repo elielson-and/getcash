511550b0d8e108877dc9b157e381134c55627b8b
Elielson Andre <elielsonandre123@gmail.com>
2024-03-10 22:36:04 -0300

    painel

---
+++ resources/js/Components/App/Sidebar.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="h-[90vh]">
+
+    </div>
+</template>
--- resources/js/Pages/Client/Dashboard.vue
+++ resources/js/Pages/Client/Dashboard.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import Sidebar from '@/Components/App/Sidebar.vue'
 import { Head } from '@inertiajs/vue3';
 import { ref } from 'vue';
 
@@ -11,17 +12,13 @@ import { ref } from 'vue';
     <Head title="Dashboard" />
 
     <AuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard client</h2>
-        </template>
+        <div class="pt-2">
+            <div class="max-w-7xl flex gap-2 mx-auto sm:px-6 lg:px-8">
+                <Sidebar class="bg-white w-80 overflow-hidden shadow-sm sm:rounded-lg" />
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="bg-white w-full h-screen overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">You're logged in!</div>
-                    {{ $page.props.auth.user.admin }}
-
-
+                    {{ $page.props.auth.user.cpf }}
                 </div>
             </div>
         </div>
