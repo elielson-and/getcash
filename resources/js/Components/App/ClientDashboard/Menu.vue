a13acbbeccba80e461aebb04f9cd8c801fdc53c1
Elielson Andre <elielsonandre123@gmail.com>
2024-03-13 01:35:22 -0300

    ,enu

---
+++ resources/js/Components/App/ClientDashboard/Menu.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-full p-4">
+        <button class="btn w-full btn-success  text-white">+ Solicitar valor</button>
+        <ul class="menu  menu-md w-full gap-1 rounded-box">
+            <li><a>Meu perfil</a></li>
+            <li><a>Documentação</a></li>
+            <li><a>Minhas solicitações</a></li>
+            <li><a>Termos</a></li>
+            <div class="divider my-0"></div>
+            <li><a class="text-red-500 hover:bg-red-100">Logout</a></li>
+        </ul>
+    </div>
+
+</template>
--- resources/js/Components/App/Sidebar.vue
+++ resources/js/Components/App/Sidebar.vue
@@ -1,5 +1,8 @@
+<script setup>
+import ClientMenu from '@/Components/App/ClientDashboard/Menu.vue';
+</script>
 <template>
     <div class="h-[90vh]">
-
+        <ClientMenu />
     </div>
 </template>
