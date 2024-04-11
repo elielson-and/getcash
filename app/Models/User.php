02c053bf5ccbec3634a1c7c9945aa0a25a9e0e72
Elielson Andre <elielsonandre123@gmail.com>
2024-04-11 11:34:52 -0300

    creating wallet entityu

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,9 @@ class User extends Authenticatable
     {
         return $this->hasOne(Document::class);
     }
+
+    public function wallet()
+    {
+        return $this->hasOne(Wallet::class);
+    }
 }
