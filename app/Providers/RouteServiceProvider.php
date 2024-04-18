35e834940a81cf260b3488ad9d648878adefd2e4
Elielson Andre <elielsonandre123@gmail.com>
2024-04-18 12:28:48 -0300

    admin new url

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/admin/dashboard';
     public const HOME_CLIENT = '/painel-do-cliente';
 
     /**
